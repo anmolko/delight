76fb5e2365ca010a236cbdc87730a41f5372a138
surajmah <suraj.canosoft@gmail.com>
2022-04-07 13:31:10 +0545

    font added

--- resources/views/frontend/pages/blogs/index.blade.php
+++ resources/views/frontend/pages/blogs/index.blade.php
@@ -11,23 +11,70 @@
     </style>
 @endsection
 @section('content')
-    <!-- Page Title -->
-    <section class="page-title" style="background-image: url('{{asset('assets/frontend/images/background/bg-17.jpg')}}');">
-        <div class="auto-container">
-            <div class="content-box">
-                <div class="content-wrapper">
-                    <div class="title">
-                        <h1>Blog </h1>
-                    </div>
-                    <ul class="bread-crumb clearfix">
-                        <li><a href="/">Home</a></li>
-                        <li>Blog </li>
-                    </ul>
-                </div>                    
+    <!--Page Title-->
+    <section class="page-title" style="background-image:url('{{asset('assets/frontend/images/background/7.jpg')}}');">
+            <div class="auto-container clearfix">
+                <h1>Latest Blog</h1>
+                <ul class="bread-crumb clearfix">
+                    <li><a href="/">Home</a></li>
+                    <li>Blog</li>
+                </ul>
             </div>
-        </div>
-    </section>
-    <!-- Page Title -->
+        </section>
+        <!--End Page Title-->
+
+        <!-- News Section -->
+        <section class="news-section">
+            <div class="auto-container">
+                <div class="row clearfix">
+
+                    <div class="content-side col-lg-8 order-2">
+                        <div class="row ">
+                            <!-- News Block -->
+                            <div class="news-block col-md-6 ">
+                                <div class="inner-box">
+                                    <div class="image-box">
+                                        <figure class="image">
+                                            <a href="blog-detail.html"><img src="images/resource/news-1.jpg" alt=""></a>
+                                        </figure>
+                                    </div>
+                                    <div class="lower-content">
+                                        <div class="post-date"><span class="far fa-calendar"></span> Jan 01, 2020</div>
+                                        <ul class="post-info">
+                                            <li><span class="far fa-user"></span> By Admin | </li>
+                                            <li><a href="#">General Carpentry</a></li>
+                                        </ul>
+                                        <h4><a href="blog-detail.html">Woodworker treak truned into furniture for exhibition.</a></h4>
+                                        <div class="btn-box"><a href="#" class="read-more">Read More <span class="fa fa-arrow-right"></span></a></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="styled-pagination text-center">
+                                <ul class="clearfix">
+                                    <li class="prev-post"><a href="#"><span class="flaticon-back-1"></span></a></li>
+                                    <li><a href="#">1</a></li>
+                                    <li><a href="#">2</a></li>
+                                    <li class="active"><a href="#">3</a></li>
+                                    <li><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li class="next-post"><a href="#"><span class="flaticon-next-1"></span> </a></li>
+                                </ul>
+                            </div>
+                           
+                        </div>
+                    </div>
+                    <!--Sidebar Side-->
+                    <div class="sidebar-side col-lg-4 ">
+                        @include('frontend.pages.blogs.sidebar')            
+                      
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--End News Section Two -->
+
 
     <!-- sidebar-page-container -->
     <section class="sidebar-page-container">
@@ -65,7 +112,6 @@
                     </div>
                 </div>
                 <aside class="col-lg-4 sidebar">
-                    @include('frontend.pages.blogs.sidebar')            
                    
                 </aside>
             </div>
--- resources/views/frontend/pages/blogs/sidebar.blade.php
+++ resources/views/frontend/pages/blogs/sidebar.blade.php
@@ -1,42 +1,57 @@
 
-<div class="blog-sidebar">
-    <div class="widget widget_search">
-        <form action="{{route('searchBlog')}}" method="get" id="searchform" class="search-form">
+<aside class="sidebar">
+
+    <!--search box-->
+    <div class="sidebar-widget search-box">
+        <form  action="{{route('searchBlog')}}" method="get" id="searchform">
             <div class="form-group">
                 <input type="search" name="s" placeholder="Your Keyword ..." oninvalid="this.setCustomValidity('Type a keyword')" oninput="this.setCustomValidity('')" required>
-                <button type="search"><i class="fas fa-search"></i></button>
+                <button type="submit"><span class="icon fa fa-search"></span></button>
             </div>
         </form>
     </div>
-    <div class="widget widget_categories">
-        <h3 class="widget-title">Categories</h3>
-        <div class="widget-content">
-            <ul class="categories-list clearfix">
-                @if(!empty($bcategories))
-                    @foreach(@$bcategories as $bcategory)
-                    <li><a class="@if(Request::url() === url('/blog/categories/'.$bcategory->slug))active @endif" href="{{url('/blog/categories/'.$bcategory->slug)}}">{{ucwords(@$bcategory->name)}}<span>{{count(@$bcategory->blogs)}}</span></a></li>
-                    @endforeach
-                @endif
-               
-            </ul>
+
+    @if(count($bcategories) > 0)
+    <!-- Categories -->
+    <div class="sidebar-widget blog-categories">
+        <div class="sidebar-title">
+            <h2>Catagories</h2>
         </div>
+        <ul class="cat-list">
+            @if(!empty($bcategories))
+                @foreach(@$bcategories as $bcategory)
+                <li class="@if(Request::url() === url('/blog/categories/'.$bcategory->slug))active @endif"><a  href="{{url('/blog/categories/'.$bcategory->slug)}}">{{ucwords(@$bcategory->name)}}<span>({{count(@$bcategory->blogs)}})</span></a></li>
+                @endforeach
+            @endif
+        </ul>
     </div>
-    <!--Popular Posts-->
+    @endif
+
+
     @if(count($latestPosts) > 0)
 
-        <div class="widget widget_popular_post">
-            <h3 class="widget-title">Latest Post</h3>
-
-                @if(!empty($latestPosts))
-                    @foreach($latestPosts as $index => $latest)
-                    <article class="post">
-                        <figure class="post-thumb"><a href="{{route('blog.single',@$latest->slug)}}"><img src="<?php if(@$latest->image){?>{{asset('/images/uploads/blog/thumb/thumb_'.@$latest->image)}}<?php }?>" alt="{{@$latest->slug}}"></a></figure>
-                        <h5><a href="{{route('blog.single',@$latest->slug)}}">{{ucwords(@$latest->title)}}</a></h5>
-                        <div class="post-info">{{date('j F, Y',strtotime(@$latest->created_at))}}</div>
-                    </article>
-                    @endforeach
-                @endif
+    <!-- Latest News -->
+    <div class="sidebar-widget latest-news">
+        <div class="sidebar-title">
+            <h2>Latest Posts</h2>
         </div>
+        <div class="widget-content">
+            @if(!empty($latestPosts))
+                @foreach($latestPosts as $index => $latest)
+                    <article class="post">
+                        <div class="post-thumb">
+                            <a href="{{route('blog.single',@$latest->slug)}}"><img src="<?php if(@$latest->image){?>{{asset('/images/uploads/blog/thumb/thumb_'.@$latest->image)}}<?php }?>" alt="{{@$latest->slug}}"></a>
+                        </div>
+                        <h3><a href="{{route('blog.single',@$latest->slug)}}">{{ucwords(@$latest->title)}}</a></h3>
+                        <div class="post-info">{{date('F j, Y',strtotime(@$latest->created_at))}}</div>
+                    </article>
+                @endforeach
+            @endif
+        
+        </div>
+    </div>
     @endif
-</div>
 
+
+
+</aside>
